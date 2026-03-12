514471adc1c631d7a20768a39d2e3995e1173c38
Mateus Sampaio Ramos <mateus.sramos77@gmail.com>
2026-03-11 23:08:34 -0300

    Security changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import { authConfig } from "./auth.config";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   trustHost: true,
+  secret: process.env.AUTH_SECRET,
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 },
 });
