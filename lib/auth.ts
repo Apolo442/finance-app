afab0226fe007647f4d39608cda436ad9c428052
Mateus Sampaio Ramos <mateus.sramos77@gmail.com>
2026-03-11 23:30:18 -0300

    Security changes

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./prisma";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import { prisma } from "./prisma";
 import { authConfig } from "./auth.config";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   trustHost: true,
   secret: process.env.AUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
+  // adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt", maxAge: 30 * 24 * 60 * 60 },
 });
